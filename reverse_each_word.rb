718cf329e71098aec10f8a8a2af5d0799209efbb
Matt Milton <mattmilton57@gmail.com>
2020-01-21 23:02:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,3 +9,11 @@ end
 
 
 
+def reverse_each_word(string)
+  array1 = string.split(" ")
+  new_array = []
+  array1.collect do |word|
+    new_array.push(word.reverse)
+  end
+  new_array.join(" ")
+end
